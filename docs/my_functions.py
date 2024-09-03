587fefe6d52e7726472e1ff8115593ab455eeac7
jkittikun <J.Kittikun@yahoo.com>
2024-09-03 22:32:59 +0700

    Add files via upload

---
+++ docs/my_functions.py
@@ -0,0 +1,12 @@
+def calculate_area(length, width):
+    """
+    Calculate the area of a rectangle.
+    """
+    return length * width
+
+PI = 3.14159
+def calculate_circle_area(radius):
+    """
+    Calculate the area of a circle.
+    """
+    return PI * radius ** 2
